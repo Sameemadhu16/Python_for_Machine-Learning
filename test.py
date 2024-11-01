7561d647de608645d86e67da56f375ec76d9ac8c
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-11-01 15:49:55 +0530

    basics in python

---
+++ test.py
@@ -0,0 +1,27 @@
+print("Hello world")
+
+# variables
+# apple = 10
+# banana = 7
+
+apple , banana = 10, 7
+result = apple + banana
+print(result)
+
+print(type("Hello"))
+print(type(10))
+print(type(False))
+
+print(2.3)
+print(2.3 + 2.3+ 2.3+ 2.3+ 2.3+ 2.3 +2.3+ 2.3+ 2.3+ 2.3+
+    2.3 + 2.3+ 2.3+ 2.3+ 2.3+ 2.3+2.3+ 2.3+ 2.3+ 2.3+
+    2.3 + 2.3+ 2.3+ 2.3+ 2.3+ 2.3+2.3+ 2.3+ 2.3+ 2.3)
+
+print(2.3*30)
+
+def dog_age():
+    dog = int(input("Enter the dog age: ")) * 7
+    print(dog)
+
+dog_age()
+
